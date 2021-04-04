2bffe24f986433d72181e0b196f662393769224b
Sandi Cardinoza <sandino.cardinoza@intellisolution.tech>
2021-04-05 01:05:27 +0800

    SecretLab;

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Lab
+                    SecretLab
                 </div>
 
                 <div class="links">
